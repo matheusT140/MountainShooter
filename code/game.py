8e454fc773466098d551ac306795e4462b46f18d
Matheus_Lopes <matheust100@hotmail.com>
2025-09-18 17:04:51 -0300

    Connecting Menu and Game classes.

--- code/game.py
+++ code/game.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from code.menu import Menu
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(800, 600))
 
     def run(self, ):
-        pass
+        menu = Menu(self.window)
+        menu.run()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close screen
+                    quit()  # end pygame
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.game import Game
 
-pygame.init()
-print('Setup start')
-screen = pygame.display.set_mode(size=(800, 600))
-print('Setup end')
-
-print('Loop start')
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # close screen
-            print('Loop end')
-            quit() # end pygame
+game = Game()
+game.run()
